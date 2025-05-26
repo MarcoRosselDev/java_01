88c224bf6167deb44ceaac41abfb9928aed71bad
Marco Rossel <andresmarcorossel@gmail.com>
2025-05-26 04:40:34 -0400

    java intro

---
+++ hello_world.java
@@ -0,0 +1,18 @@
+// The first line defines a class called Main.
+public class Main {
+    /* 
+    public --------> means that anyone can access it.
+    static --------> means that you can run this method without creating an instance of Main.
+    void ----------> means that this method doesn't return any value.
+    main ----------> is the name of the method.
+     */
+    public static void main(String[] args) {
+        /* 
+        System----> is a pre-defined class that Java provides us and it holds some useful methods and variables.
+        out-------> is a static variable within System that represents the output of your program (stdout).
+        println---> is a method of out that can be used to print a line.    
+        
+         */
+        System.out.println("Hello, World!");
+    }
+}
---
+++ variables_and_types.java
@@ -0,0 +1,30 @@
+/* Lista de primitivos en java
+byte (number, 1 byte)
+short (number, 2 bytes)
+int (number, 4 bytes)
+long (number, 8 bytes)
+float (float number, 4 bytes)
+double (float number, 8 bytes)
+char (a character, 2 bytes)
+boolean (true or false, 1 byte)
+ */
+
+public class Main {
+    public static void main(String[] args) {
+      /* para agregar una variable se deve espesificar el tipo asi: */
+      int myNumber = 5; // entero
+      double d = 4.5; // numero doble flotante (flotante mas presiso y mas grande que float normal)
+      float f = (float) 4.5; // flotante chiquito
+      float fc = 4.5f; // la f es una avreviacion de casting float (chiquito abreviado)
+
+
+      /* los strings no son tan simples */
+      char c = 'g';
+      // Create a string with a constructor
+      String s1 = new String("Who let the dogs out?");        // String object stored in heap memory
+      // Just using "" creates a string, so no need to write it the previous way.
+      String s2 = "Who who who who!";                         // String literal stored in String pool
+      // Java defined the operator + on strings to concatenate:
+      String s3 = s1 + s2;
+    }
+}
